e229bad14fec62df26b9cce410ec59004abd190c
Wabyoona <maxwell@gmail.com>
2022-09-09 17:23:27 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,33 +1,19 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Description: print all aplhabet letters
-
  *
-
  * Return: Always 0 (Success)
-
 */
 
-
-
 int main(void)
-
 {
 
 int n = 0;
 
-
-
 while (n < 10)
-
 {
 
 printf("%i", n);
