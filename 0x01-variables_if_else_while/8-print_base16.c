9b7d9548225771b76da61226b274c3fba5577a44
Wabyoona <maxwell@gmail.com>
2022-09-10 12:20:49 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,16 @@
 */
 int main(void)
 {
-int num;
-char letter;
+char c;
 
-for (num = 0; num < 10; num++)
-putchar((num % 10) + 0);
-
-for (letter = 'a'; letter <= 'f'; letter++)
-putchar(letter);
-
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{                                                      
+putchar(c);                                                               
+}   
 putchar('\n');
 
 return (0);
