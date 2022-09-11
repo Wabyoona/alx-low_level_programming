efb11ce851f3b47e969c06d32cb469f37ca788f8
Wabyoona <maxwell@gmail.com>
2022-09-11 13:10:23 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
- *  Description: print all possible different
- *  combinations of two digits.
+ * Description: print all possible different
+ * combinations of two digits.
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
