bc3f4653279566456044856bb549e168e46210af
Wabyoona <maxwell@gmail.com>
2022-09-13 10:49:58 -0700

    task 0

---
+++ 0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* program that prints _putchar, followed by a new line
+*
+* Return: always 0(Success)
+*/
+
+int  main (void)
+{
+printf("_putchar\n");
+return 0;
+}
