55266ff4bb6db807a2cec2e868d6f5877b503c78
Wabyoona <maxwell@gmail.com>
2022-09-22 11:53:51 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,18 +1,10 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * leet - Encodes a string to 1337.
- *
- *   * @str: The string to be encoded.
- *
- *    *
- *
- *     * Return: A pointer to the encoded string.
- *
- *      */
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ * Return: A pointer to the encoded string.
+ */
 
 char *leet(char *str)
 
