db60d3d95c98e9a7875347734a5555c506c18038
Wabyoona <maxwell@gmail.com>
2022-09-23 01:36:58 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
+ * main - check the code
  * print_number - Prints an integer.
  * @n: The integer to be printed.
+ * Return: Always 0.
  */
 
 void print_number(int n)
-
 {
 
 		unsigned int num = n;
