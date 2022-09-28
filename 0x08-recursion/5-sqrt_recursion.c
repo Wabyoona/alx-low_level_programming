6a878e3c30be31a3b3d44e7416490bc0ed0954d2
Wabyoona <maxwell@gmail.com>
2022-09-28 15:47:39 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,23 +13,12 @@ int _sqrt_recursion(int n);
  */
 
 int find_sqrt(int num, int root)
-
 {
-
-		if ((root * root) == num)
-
-					return (root);
-
-
-
-			if (root == num / 2)
-
-						return (-1);
-
-
-
-				return (find_sqrt(num, root + 1));
-
+if ((root * root) == num)
+return (root);
+if (root == num / 2)
+return (-1);
+return (find_sqrt(num, root + 1));
 }
 
 /**
@@ -39,27 +28,12 @@ int find_sqrt(int num, int root)
  * Return: If n has a natural square root - the natural square root of n.
  *         If n does not have a natural square root - -1.
  */
-
 int _sqrt_recursion(int n)
-
 {
-
-		int root = 0;
-
-
-
-			if (n < 0)
-
-						return (-1);
-
-
-
-				if (n == 1)
-
-							return (1);
-
-
-
-					return (find_sqrt(n, root));
-
+int root = 0;
+if (n < 0)
+return (-1);
+if (n == 1)
+return (1);
+return (find_sqrt(n, root));
 }
